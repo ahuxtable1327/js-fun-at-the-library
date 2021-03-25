99036f10526339826bbf039957b9271acabb6bd8
Ashton Huxtable <aehuxtable@gmail.com>
2021-03-24 20:55:11 -0600

    Add buildMainCharacter function and character onject

--- src/book.js
+++ src/book.js
@@ -1,11 +1,25 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return (`The ${bookTitle}`);
+};
+
+
+function buildMainCharacter(characterName, age, preferredPronouns){
+  var character = {
+    name: characterName,
+    age: age,
+    pronouns: preferredPronouns,
+  }
+  return character;
+};
+
+function saveReview() {
+  var reviews = [];
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
