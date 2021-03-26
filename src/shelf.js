86a159505a967dd1d651e6e7f4caaa4878c5843a
Ashton Huxtable <aehuxtable@gmail.com>
2021-03-26 15:57:08 -0600

    Create shelfBook and unshelfBook functions

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,23 @@
+function shelfBook (book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (title === shelf[i].title) {
+      shelf.splice([i],1);
+    }
+  }
+}
+// sciFiShelf[i].title
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
